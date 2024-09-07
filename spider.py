3ebc8d9918718d2706be486880c9bb768003da1e
song cai <fhtest_cs@163.com>
2024-09-07 15:47:51 +0800

    update

--- spider.py
+++ spider.py
@@ -12,9 +12,10 @@
 
 ### 过滤tag  
 
-import requests
 import time
 from jinja2 import Template
+import asyncio
+import aiohttp
 
 HOST = "zhblogs.ohyee.cc"
 
@@ -41,39 +42,44 @@ FEED_TEMPLATE='''<?xml version="1.0" encoding="UTF-8"?>
 </opml>
 '''
 
-feed = set()
-blog_with_category = {}
 
-with requests.Session() as session:
-    session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0'})
-    resp = session.get(f"https://{HOST}/api/tags")
-    try:
-        tags = resp.json()['data']
-        EXPECT = list(set(EXPECT) & set(tags))
-        FILTER = list(set(FILTER) & set(FILTER))
-    except:
-        raise
-
-    offset, size, total = 0, 10, 10
+async def fetch(session, tag):
     category_blogs = []
+    offset, total = 0, 10
+    for offset in range(0, total, 10):
+        async with session.get(f"https://{HOST}/api/blogs", params={'search':'', 'tags': tag, 'offset': offset, 'size': 10}) as response:
+            data = await response.json()
+            total, blogs = data["data"]["total"], data["data"]["blogs"]
+            category_blogs.extend([(blog["name"], blog["url"], blog["feed"]) for blog in blogs if time.time() - blog["update_time"]<365*24*3600 and not set(blog["tags"]) & set(FILTER)])
+    return category_blogs
 
-    for tag in EXPECT:
-        for offset in range(0, total, 10):
-            try:
-                resp = session.get(f"https://{HOST}/api/blogs",
-                               params={'search':None, 'tags': tag, 'offset': offset, 'size': 10})
-                data = resp.json()["data"]
-                total, blogs = data["total"], data["blogs"]
-                category_blogs.extend([(blog["name"], blog["url"], blog["feed"]) for blog in blogs if time.time() - blog["update_time"]<365*24*3600])
-            except:
-                raise
 
-        blog_with_category[tag] = set(category_blogs) - feed
-        feed = set(category_blogs) | feed
+async def main():
+    feed = set()
+    global EXPECT, FILTER
+    blog_with_category = {}
+    async with aiohttp.ClientSession() as session:
+        session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0'})
+        try:
+            resp = await session.get(f"https://{HOST}/api/tags")
+            data = await resp.json()
+            tags = data['data']
+            EXPECT = list(set(EXPECT) & set(tags))
+            FILTER = list(set(FILTER) & set(FILTER))
+        except:
+            raise
+        tasks = [fetch(session, tag) for tag in EXPECT]
 
-template = Template(FEED_TEMPLATE)
-rendered = template.render(data=blog_with_category)
+        category_blogs = await asyncio.gather(*tasks)
+        for tag, blogs in zip(EXPECT, category_blogs):
+            blog_with_category[tag] = list(set(blogs) - feed)
+            feed = feed | set(blogs)
+    
+    template = Template(FEED_TEMPLATE)
+    rendered = template.render(data=blog_with_category)
+    with open('feed.opml', 'w') as opml:
+        opml.write(rendered)
 
-with open('feed.opml', 'w') as opml:
-    opml.write(rendered)
+if __name__ == '__main__':
+    asyncio.run(main())
 
